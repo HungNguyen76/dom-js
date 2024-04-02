1679f1dde83296c073335a26a20b5a3bb0527b9b
NguyenMinhHung <nmhung.7676@gmail.com>
2024-04-02 08:48:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## DOM có 3 thành phần chính
+# 1. Element
+# 2. Attribute
+# 3 . Text
+
+# Ex: <a href="https://google.com">Google</a>
+# element: thẻ a
+# attribute: href
+# text: google
+
+## Các phương thức truy xuất phần tử dom
+# document.getElementById(id) (trả về object)
+# document.getElementsByTagName(tag_name) (trả về mảng)
+# EX: <p id="foo:bar" class="title"></p> => document.querySelector("#foo\\:bar")
+
+## Sự khác nhau giữa innerHTML và textContent
+# innertHTML : thay đổi cấu trúc của element
+# textContent: thay đổi nội dung văn bản của element
+
+
+## Event trong DOM
+# sự kiện chuột: onmouseover, onmouseout
+# sự kiện bàn phím: onkeyup, onkeydown, onkeypress
+
+## Form
---
+++ index.js
@@ -0,0 +1,92 @@
+// const p = document.getElementById("demo")
+// console.log("p", typeof p)
+
+// p.innerHTML = "Hello cac ban"
+
+// const tag = document.getElementsByTagName("p")
+// for (var el in tag) {
+//     tag[el].innerHTML = "Hello cac ban"
+// }
+
+// const title = document.getElementsByClassName("title")
+// console.log("title", title)
+
+const title = document.querySelector("#title")
+title.innerHTML = `
+    <button>Submit</button>
+`
+
+const linkGG = document.getElementById("linkGG")
+linkGG.style.color = "red"
+linkGG.style.textDecoration = "none"
+
+//thêm attribute cho element
+title.class = "title"
+
+console.log("title", title)
+
+const inp = document.getElementById("submit")
+inp.placeholder = "Your full name"
+
+//setAttribute => tham số 1: attribute, tham số 2: value
+inp.setAttribute("placeholder", "Your password")
+
+//Các phương thức để thao tác với phần tử trong DOM
+
+
+// const congTinh = document.getElementById("congTinh")
+// const h3 = document.createElement("h3")
+
+// h3.textContent = "Cong Tinh yeu mau hong, ghet su gia doi"
+// h3.style.color = "pink"
+// congTinh.appendChild(h3)
+
+document.body.onload = function() {
+    // const newDiv = document.createElement("div")
+    // const newContent = document.createTextNode("Cong Tinh dep trai")
+    // newDiv.appendChild(newContent)
+
+    // const currentDiv = document.getElementById('div1')
+    // document.body.insertBefore(newDiv, currentDiv)
+
+    const tabl = document.createElement("table")
+    tabl.innerHTML = `
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+        </tr>
+        <tbody>
+            <td>1</td>
+            <td>Nguyen Minh Hung</td>
+        </tbody>
+    
+    `
+    tabl.style.border = "1px solid #000"
+    tabl.style.marginTop = "15px"
+    
+    const div1 = document.getElementById("div1")
+    div1.appendChild(tabl)
+};
+
+// const btnEl = document.getElementById("btn")
+// btnEl.addEventListener("click", function(event) {
+    
+// })
+
+const cT = document.getElementById("congTinh")
+cT.addEventListener("click", function() {
+    cT.innerHTML = "Oopps!"
+})
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date()
+}
+
+function mOver() {
+   const listDiv= document.getElementsByTagName("div")[1]
+   listDiv.innerHTML = "Thank You "
+}
+
+function mOut(obj) {
+    obj.innerHTML = "Mouse over me"
+}
